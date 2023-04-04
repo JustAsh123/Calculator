85330e40e55bae63c47be9e7263d42f1327a1043
JustAsh123 <ashmit1268@gmail.com>
2023-04-04 22:01:48 +0530

    final

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function add_to_screen(x){
     }
     document.getElementById("screen").value = document.getElementById("screen").value+x
 }
+
+function calculate(){
+    let eq = document.getElementById("screen").value;
+    let res = eval(eq);
+    document.getElementById("screen").placeholder = res;
+    document.getElementById("screen").value = "";
+}

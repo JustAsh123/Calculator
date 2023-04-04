52070d64a84c08cda4d54e4581bee61942f04427
JustAsh123 <ashmit1268@gmail.com>
2023-04-04 20:49:48 +0530

    z

--- index.js
+++
@@ -1,9 +0,0 @@
-function changeViewportScale() {
-    // Get the viewport meta tag
-    var metaViewport = document.querySelector('meta[name="viewport"]');
-  
-    metaViewport.setAttribute('content', 'width=device-width, initial-scale=1.75');
-}
-  // Call the function on page load and on resize
-  window.addEventListener('load', changeViewportScale);
-  window.addEventListener('resize', changeViewportScale);
